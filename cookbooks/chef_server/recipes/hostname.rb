f7083ab074e92ab1b8ab9b7d86316d9d535407ef
Dan <dxi02@acuitysso.com>
2018-07-16 17:46:14 -0400

    adding back hostnamectl

--- cookbooks/chef_server/recipes/hostname.rb
+++ cookbooks/chef_server/recipes/hostname.rb
@@ -9,7 +9,7 @@ hostsfile_entry node['chef_server']['chef_server_ip'] do
     action :append 
 end
 
-# execute 'set host name' do
-#     command "hostnamectl set-hostname #{node['chef_server']['chef_server_host']}"
-#     action :run
-# end
+execute 'set host name' do
+    command "hostnamectl set-hostname #{node['chef_server']['chef_server_host']}"
+    action :run
+end
